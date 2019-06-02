9293c6d1992f897b9bf4a44e6aed3a0d9e92fdda
The Natural <philip.smalls86@gmail.com>
2019-06-02 20:58:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   low_val = nil 
   
   name_hash.each do |key, value|
-    if low_val == nil || low_val > val 
+    if low_val == nil ||  
       low_val = value 
       low_key = key 
     end 
